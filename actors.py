32f962bf650e94c3f49924c586d2dbcf4931ce65
Jose Gonzalo Scali <gonzalojoses@hotmail.com>
2023-08-14 12:21:01 -0300

    fix

--- actors.py
+++ actors.py
@@ -3,11 +3,11 @@ from pykka import ThreadingActor
 
 
 class ActorUno(ThreadingActor):
-    def on_receive(self, funcion: function):
-        funcion()
+    def on_receive(self, funcion):
+        print(funcion())
 
 class ActorDos(ThreadingActor):
-    def on_receive(self, funcion: function):
-        funcion()
+    def on_receive(self, funcion):
+        print(funcion())
 
 
